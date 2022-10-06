fce7dc42f8a1b3e4741b9b1282dbf54228b39403
MarkHenderson02 <114543984+MarkHenderson02@users.noreply.github.com>
2022-10-06 09:46:41 +0800

    Circle Counting Program

---
+++ Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnHitung = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tbJarijari = New System.Windows.Forms.TextBox()
+        Me.tbLuas = New System.Windows.Forms.TextBox()
+        Me.tbKeliling = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(63, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(277, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hitung Luas dan Keliling Lingkaran"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(76, 125)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Jari-jari"
+        '
+        'btnHitung
+        '
+        Me.btnHitung.BackColor = System.Drawing.Color.Maroon
+        Me.btnHitung.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHitung.ForeColor = System.Drawing.SystemColors.InactiveBorder
+        Me.btnHitung.Location = New System.Drawing.Point(128, 199)
+        Me.btnHitung.Name = "btnHitung"
+        Me.btnHitung.Size = New System.Drawing.Size(112, 27)
+        Me.btnHitung.TabIndex = 3
+        Me.btnHitung.Text = "Hitung"
+        Me.btnHitung.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(76, 269)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Luas"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(76, 295)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 17)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Keliling"
+        '
+        'tbJarijari
+        '
+        Me.tbJarijari.Location = New System.Drawing.Point(137, 124)
+        Me.tbJarijari.Name = "tbJarijari"
+        Me.tbJarijari.Size = New System.Drawing.Size(163, 20)
+        Me.tbJarijari.TabIndex = 8
+        '
+        'tbLuas
+        '
+        Me.tbLuas.Location = New System.Drawing.Point(137, 268)
+        Me.tbLuas.Name = "tbLuas"
+        Me.tbLuas.Size = New System.Drawing.Size(163, 20)
+        Me.tbLuas.TabIndex = 9
+        '
+        'tbKeliling
+        '
+        Me.tbKeliling.Location = New System.Drawing.Point(137, 295)
+        Me.tbKeliling.Name = "tbKeliling"
+        Me.tbKeliling.Size = New System.Drawing.Size(163, 20)
+        Me.tbKeliling.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(402, 387)
+        Me.Controls.Add(Me.tbKeliling)
+        Me.Controls.Add(Me.tbLuas)
+        Me.Controls.Add(Me.tbJarijari)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnHitung)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form Hitung Lingkaran"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnHitung As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents tbJarijari As TextBox
+    Friend WithEvents tbLuas As TextBox
+    Friend WithEvents tbKeliling As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim r As Double
+        Const phi = 3.14
+
+        r = Val(tbJarijari.Text)
+        tbLuas.Text = phi * r * r
+        tbKeliling.Text = 2 * phi * r
+
+    End Sub
+End Class
